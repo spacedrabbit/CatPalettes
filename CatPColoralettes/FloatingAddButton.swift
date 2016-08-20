238d7ffc6d9277e101e3de4cfb3fd6eaa6debeb9
Louis Tur <louis.tur@gmail.com>
2016-08-20 18:52:57 -0400

    a little bit of clean up to how the button will be implemented in the future

--- CatPColoralettes/FloatingAddButton.swift
+++ CatPColoralettes/FloatingAddButton.swift
@@ -9,21 +9,21 @@
 import Foundation
 import UIKit
 
-internal class FloatingButton: UIControl {
+internal class FloatingButton: UIView {
   internal static let ButtonSize: CGFloat = 48.0
   internal static var CornerRadius: CGFloat {
     return FloatingButton.ButtonSize * 0.5
   }
-  
-  internal var plusIcon: UIImage = UIImage(named: "plus")!
-  internal var imageView: UIImageView!
+
+  lazy internal var imageView: UIImageView = {
+    let view: UIImageView = UIImageView(image: UIImage(named: "plus")!.imageWithRenderingMode(.AlwaysTemplate))
+    view.tintColor = AppColors.DefaultTitleText
+    view.contentMode = .ScaleAspectFit
+    return view
+  }()
   
   override init(frame: CGRect) {
     super.init(frame: frame)
-    plusIcon.imageWithRenderingMode(.AlwaysTemplate)
-    self.imageView = UIImageView(image: plusIcon)
-    self.imageView.tintColor = AppColors.DefaultTitleText
-    self.imageView.contentMode = .ScaleAspectFit
     
     self.setupViewHierarchy()
     self.configureConstraints()
@@ -43,7 +43,8 @@ internal class FloatingButton: UIControl {
     }
     
     self.imageView.snp_makeConstraints { (make) in
-      make.edges.equalTo(self)
+      make.center.equalTo(self)
+      make.size.equalTo(CGSize(width: FloatingButton.CornerRadius, height: FloatingButton.CornerRadius))
     }
   }
   
@@ -52,8 +53,12 @@ internal class FloatingButton: UIControl {
     self.addSubview(imageView)
     
     self.clipsToBounds = true
+    self.blurryBackgroundView.alpha = 0.5
     self.layer.cornerRadius = FloatingButton.CornerRadius
   }
   
-  lazy internal var blurryBackgroundView: UIVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .Light))
+  // TODO: This will need some style adjustment
+  lazy internal var blurryBackgroundView: UIVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .ExtraLight))
+  // TODO: switch image view to be a UIControl and add image to it as subview. then adjust the control states
+  
 }
