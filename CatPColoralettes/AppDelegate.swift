a73dc879cafea45bb5191abddf9bfbab3adfe94a
Louis Tur <louis.tur@gmail.com>
2016-08-20 22:04:56 -0400

    quick switch over to using a uiviewcontroller over tableVC

--- CatPColoralettes/AppDelegate.swift
+++ CatPColoralettes/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     
-    let rootVC = PaletteTableViewController()
+//    let rootVC = PaletteTableViewController()
+    let rootVC = PaletteViewController()
     let navigationController: UINavigationController = UINavigationController(rootViewController: rootVC)
     navigationController.navigationBar.barTintColor = AppColors.DefaultBackground
     navigationController.navigationBar.titleTextAttributes = [
